f05ce15b66476e91c656e1eed0602e82edd4bc3f
scottmerseburg <112166703+scottmerseburg@users.noreply.github.com>
2023-04-15 11:32:46 +0200

    Camera size max fixed

--- Assets/Scripts/ColliderTest.cs
+++ Assets/Scripts/ColliderTest.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class ColliderTest : MonoBehaviour
 {
     public PlayerMovement playerMovement;
+    public PlayerMovement playerMovement2;
+
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.collider.gameObject.tag == "Player")
+        if (collision.collider.gameObject.tag == "Player" || collision.collider.gameObject.tag == "Player2") 
         {
             playerMovement.KBCounter = playerMovement.KBTotalTime;
+            playerMovement2.KBCounter = playerMovement2.KBTotalTime;
 
             if (collision.transform.position.x <= transform.position.x)
             {
                 playerMovement.KnockFromRight = true;
+                playerMovement2.KnockFromRight = true;
                 Debug.Log("Hit");
 
 
@@ -22,6 +26,7 @@ public class ColliderTest : MonoBehaviour
             if (collision.transform.position.x > transform.position.x)
             {
                 playerMovement.KnockFromRight = false;
+                playerMovement2.KnockFromRight = false;
                 Debug.Log("Hit");
 
             }
